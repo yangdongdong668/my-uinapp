5c0145014f883936571aeaecc88e171341c56f0b
yangdongdong <yangdongdong920504@163.com>
2020-10-19 11:16:55 +0800

    首次提交

---
+++ pages/contact/contact.vue
@@ -0,0 +1,56 @@
+<template>
+	<view class="contact">
+		<image class="img" src="http://www.itcast.cn/2018czydz/images/gywmban.jpg" mode=""></image>
+		<view class="info">
+			<view @click="phone">
+				联系电话：400-618-9090 ( 点击拨打 )
+			</view>
+		</view>
+		<map class="map" :latitude="latitude" :longitude="longitude" :scale="scale" :markers="markers"></map>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				longitude: 120.363172,
+				latitude: 30.312212,
+				scale: 13,
+				markers: [{
+					longitude: 120.363172,
+					latitude: 30.312212,
+					iconPath: '../../static/icon/home-active.png',
+					width: 30,
+					height: 30
+				}]
+			}
+		},
+		methods: {
+			phone() {
+				uni.makePhoneCall({
+					phoneNumber: '400-618-9090'
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.img {
+		width: 750rpx;
+		height: 320rpx;
+	}
+	.info{
+		padding:10rpx 20rpx;
+		font-size:30rpx;
+		view{
+			line-height:80rpx;
+			border-bottom:1px solid #eee;
+		}
+	}
+	.map {
+		width: 750rpx;
+		height: 750rpx;
+	}
+</style>
---
+++ pages/goods-detail/goods-detail.vue
@@ -0,0 +1,180 @@
+<template>
+	<view class="goods_detail">
+		<swiper circular :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+			<swiper-item v-for="(item,index) in swipers" :key="index">
+				<image :src="item.src" mode=""></image>
+			</swiper-item>
+		</swiper>
+		<view class="box box1">
+			<view class="price">
+				<text>￥{{info.sell_price}}</text>
+				<text>￥{{info.market_price}}</text>
+			</view>
+			<view class="goods_name">
+				{{info.title}}
+			</view>
+		</view>
+		<view class="box box2">
+			<view>
+				货号：{{info.goods_no}}
+			</view>
+			<view>
+				库存：{{info.stock_quantity}}
+			</view>
+		</view>
+		<view class="box3">
+			<view class="tit">
+				详情介绍
+			</view>
+			<view class="content">
+				<rich-text :nodes="content"></rich-text>
+			</view>
+		</view>
+		<view class="goods_nav">
+			<uni-goods-nav :fill="true" :options="options" :button-group="buttonGroup" @click="onClick" @buttonClick="buttonClick"></uni-goods-nav>
+		</view>
+	</view>
+</template>
+
+<script>
+	import uniGoodsNav from '@/components/uni-goods-nav/uni-goods-nav.vue'
+	import parseHtml from '@/util/parseHtml.js'
+	export default {
+		data() {
+			return {
+				id: 0,
+				swipers: [],
+				info: {},
+				content: [],
+				options: [{
+					icon: 'headphones',
+					text: '客服'
+				}, {
+					icon: 'shop',
+					text: '店铺'
+				}, {
+					icon: 'cart',
+					text: '购物车',
+					info: 12
+				}],
+				buttonGroup: [{
+						text: '加入购物车',
+						backgroundColor: '#ff0000',
+						color: '#fff'
+					},
+					{
+						text: '立即购买',
+						backgroundColor: '#ffa200',
+						color: '#fff'
+					}
+				]
+			}
+		},
+		components: {
+			uniGoodsNav
+		},
+		methods: {
+			async getSwipers() {
+				const res = await this.$myRequest({
+					url: '/api/getthumimages/' + this.id
+				})
+				this.swipers = res.data.message
+			},
+			async getDetailInfo() {
+				const res = await this.$myRequest({
+					url: '/api/goods/getinfo/' + this.id
+				})
+				this.info = res.data.message[0]
+			},
+			async getDetailContent() {
+				const res = await this.$myRequest({
+					url: '/api/goods/getdesc/' + this.id
+				})
+				this.content = parseHtml(res.data.message[0].content)
+				console.log(this.content)
+			},
+			onClick(e) {
+				console.log(e)
+			},
+			buttonClick(e) {
+				console.log(e)
+			}
+		},
+		onLoad(options) {
+			this.id = options.id
+			this.getSwipers()
+			this.getDetailInfo()
+			this.getDetailContent()
+		}
+	}
+</script>
+
+<style lang="scss">
+	.goods_detail {
+		swiper {
+			height: 700rpx;
+			width: 750rpx;
+
+			image {
+				width: 750rpx;
+				height: 700rpx;
+			}
+		}
+
+		.box {
+			border-bottom: 10rpx solid #eee;
+		}
+
+		.box1 {
+			padding: 10px;
+
+			.price {
+				font-size: 35rpx;
+				color: $shop-color;
+				line-height: 80rpx;
+
+				text:nth-child(2) {
+					color: #ccc;
+					font-size: 28rpx;
+					text-decoration: line-through;
+					margin-left: 20rpx;
+				}
+			}
+
+			.goods_name {
+				font-size: 32rpx;
+				line-height: 60rpx;
+			}
+		}
+
+		.box2 {
+			padding: 0 10px;
+			font-size: 32rpx;
+			line-height: 70rpx;
+		}
+
+		.box3 {
+			padding-bottom: 50px;
+
+			.tit {
+				font-size: 32rpx;
+				padding-left: 10px;
+				border-bottom: 1px solid #eee;
+				line-height: 70rpx;
+			}
+
+			.content {
+				padding: 10px;
+				font-size: 28rpx;
+				color: #333;
+				line-height: 50rpx;
+			}
+		}
+
+		.goods_nav {
+			position: fixed;
+			bottom: 0;
+			width: 100%;
+		}
+	}
+</style>
